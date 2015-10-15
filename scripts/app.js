64f1671aad6f12d235729ab86c81955deabd17d1
Evan Cummings <emcummings@gmail.com>
2015-10-14 22:59:00 -0400

    Awwyiss

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'ab0TSkLe-E0', start: 3 };
+	var options = { videoId: 'FG0fTKAqZ5g', start: 6 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
